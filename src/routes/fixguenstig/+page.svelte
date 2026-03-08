8e52bb324818bbc714fab6aa6ff27bbe7c6a0ea3
Kai-Uwe Chromik <kai.chromik@online.de>
2026-03-08 08:38:29 +0100

    feat: update folding length notes based on format selection for improved clarity

--- src/routes/fixguenstig/+page.svelte
+++ src/routes/fixguenstig/+page.svelte
@@ -1108,7 +1108,14 @@ const {
 										<path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
 										<path d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
 									</svg>
-									<strong>Hinweis zu den Falzlängen:</strong> 2 x 100 mm + Einklapper 97 mm
+									<strong>Hinweis zu den Falzlängen:</strong>
+									{#if getFormatData(format)?.id === 'format-a6'}
+										2 x 105 mm + Einklapper 102 mm
+									{:else if getFormatData(format)?.id === 'format-a5'}
+										2 x 148 mm + Einklapper 145 mm
+									{:else}
+										2 x 100 mm + Einklapper 97 mm
+									{/if}
 								</div>
 							{:else if falzart === 'Zickzackfalz'}
 								<div class="material-info" style="margin-top: 0.75rem;">
@@ -1116,7 +1123,14 @@ const {
 										<path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
 										<path d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
 									</svg>
-									<strong>Hinweis zu den Falzlängen:</strong> 3 x 99 mm
+									<strong>Hinweis zu den Falzlängen:</strong>
+									{#if getFormatData(format)?.id === 'format-a6'}
+										3 x 105 mm
+									{:else if getFormatData(format)?.id === 'format-a5'}
+										3 x 148 mm
+									{:else}
+										3 x 99 mm
+									{/if}
 								</div>
 							{/if}
 						</div>
